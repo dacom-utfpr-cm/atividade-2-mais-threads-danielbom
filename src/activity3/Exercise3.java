cd8b41f942746b0ef0b40f250aea4c460c1e0d27
danielbom <daniel.rodrigues.45@hotmail.com>
2019-09-02 15:52:47 -0300

    Update Exercise3.java
    Using shered atomic variable to produce number to each thread process your task.

--- src/activity3/Exercise3.java
+++ src/activity3/Exercise3.java
@@ -1,5 +1,6 @@
 package activity3;
 
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.IntStream;
 
 /**
@@ -12,18 +13,24 @@ import java.util.stream.IntStream;
  */
 public class Exercise3 {
 
+	private static long getNextNumber(AtomicLong number) {
+		long prev, next;
+		do {
+			prev = number.get();
+			next = prev + 1;
+		} while (!number.compareAndSet(prev, next));
+		return next;
+	}
+	
 	public static void main(String[] args) {
-		long range = 10_000, max = 100_000;
+		long max = 100_000;
+		AtomicLong number = new AtomicLong(0);
 
-		IntStream.rangeClosed(0, (int) (max / range) - 1).forEach(i -> {
+		IntStream.rangeClosed(0, 7).forEach(i -> {
 			new Thread(() -> {
-				long p = 0;
-				for (long j = 0; j < range; j++) {
-					long k = (range * i) + j;
-					if (Primes.isPrime(k)) {
-						System.out.println("Prime (" + p + ")");
-					}
-				}
+				for (long k = getNextNumber(number); k < max; k = getNextNumber(number))
+					if (Primes.isPrime(k))
+						System.out.println("Prime (" + k + ")");
 			}).start();
 		});
 	}
